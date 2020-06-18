4132704e2b50309891e12cf9dc1749b5d5fa2a67
2020上機考版本0(林昱廷) <andy3239@gmaul.com>
2020-06-18 09:42:46 +0800

    final

--- app/src/main/java/tw/edu/pu/edu/s1071483/bread/six.java
+++ app/src/main/java/tw/edu/pu/edu/s1071483/bread/six.java
@@ -1,13 +1,13 @@
 package tw.edu.pu.edu.s1071483.bread;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class six extends AppCompatActivity {
     EditText quantity;
@@ -26,17 +26,24 @@ public class six extends AppCompatActivity {
                     int fq = Integer.parseInt(quantity.getEditableText().toString());
                     int sugar;
                     int flour;
+                    int egg;
+                    int s;
                     TextView result = (TextView)findViewById(R.id.tv5);
-                    result.setText("草莓"+"適量");
+                    s=fq*10;
+                    result.setText("草莓醬"+s+"g");
 
 
                     TextView result1 = (TextView)findViewById(R.id.tv6);
-                    sugar=fq*20;
+                    sugar=fq*120;
                     result1.setText("糖"+sugar+"g");
 
                     TextView result2 = (TextView)findViewById(R.id.tv7);
-                    flour=fq*110;
+                    flour=fq*460;
                     result2.setText("麵粉"+flour+"g");
+
+                    TextView result3 = (TextView)findViewById(R.id.tv8);
+                    egg=fq*50;
+                    result3.setText("雞蛋"+egg+"g");
 
                 }
             }
